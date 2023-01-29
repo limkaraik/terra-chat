37a7cbab9cd0de7cd9a86cb36c176a52076b0c25
limkaraik <limkaraik@hotmail.com>
2023-01-30 03:16:15 +0800

    get sent and received

--- x/chat/keeper/query_get_received.go
+++ x/chat/keeper/query_get_received.go
@@ -4,7 +4,10 @@ import (
 	"context"
 
 	"chat/x/chat/types"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -14,10 +17,27 @@ func (k Keeper) GetReceived(goCtx context.Context, req *types.QueryGetReceivedRe
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	var texts []types.Text
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	store := ctx.KVStore(k.storeKey)
+	textStore := prefix.NewStore(store, types.KeyPrefix(types.TextKey))
+	receiver := req.Receiver
 
-	return &types.QueryGetReceivedResponse{}, nil
+	pageRes, err := query.Paginate(textStore, req.Pagination, func(key []byte, value []byte) error {
+		var text types.Text
+		if err := k.cdc.Unmarshal(value, &text); err != nil {
+			return err
+		}
+		if text.Receiver == receiver {
+			texts = append(texts, text)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryGetReceivedResponse{Text: texts, Pagination: pageRes}, nil
 }
--- x/chat/keeper/query_get_sent.go
+++ x/chat/keeper/query_get_sent.go
@@ -4,7 +4,10 @@ import (
 	"context"
 
 	"chat/x/chat/types"
+
+	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -14,10 +17,27 @@ func (k Keeper) GetSent(goCtx context.Context, req *types.QueryGetSentRequest) (
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
+	var texts []types.Text
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Process the query
-	_ = ctx
+	store := ctx.KVStore(k.storeKey)
+	textStore := prefix.NewStore(store, types.KeyPrefix(types.TextKey))
+	sender := req.Sender
 
-	return &types.QueryGetSentResponse{}, nil
+	pageRes, err := query.Paginate(textStore, req.Pagination, func(key []byte, value []byte) error {
+		var text types.Text
+		if err := k.cdc.Unmarshal(value, &text); err != nil {
+			return err
+		}
+		if text.Receiver == sender {
+			texts = append(texts, text)
+		}
+		return nil
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &types.QueryGetSentResponse{Text: texts, Pagination: pageRes}, nil
 }
